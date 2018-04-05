2d2f9dec5f6d17267d73f1defb5d4de3958654ca
Austen Casey <austenc@rodete-desktop-imager.corp.google.com>
2018-04-05 14:54:14 -0700

    .

--- twilio_fn.js
+++ twilio_fn.js
@@ -1,29 +1,26 @@
 exports.handler = function(context, event, callback) {
-  var error_log = '';
-  var success_log = '';
+  var paste = require('better-pastebin');
+  paste.setDevKey(context.PASTEBIN_API_KEY);
+  paste.login(
+    context.PASTEBIN_USERNAME,
+    context.PASTEBIN_PASSWORD,
+    function(success, data) {
+      if(!success) {
+        console.log('Login Failed (' + data + ')');
+        return false;
+      }
 
-  var jsdom = require('jsdom');
-  const { JSDOM } = jsdom;
-  const { window } = new JSDOM('<html></html>');
-  var $ = require('jquery')(window);
-
-  $.ajax({
-    url: context.JSONBIN_URL,
-    type: 'PUT',
-    contentType: 'application/json',
-    headers: {
-      'secret-key': context.JSONBIN_KEY
-    },
-    data: JSON.stringify({ message: "words" }),
-    success: (data) => {
-      console.log(data);
-      success_log = data;
-    },
-    error: (err) => {
-      console.log(err.responseJSON);
-      error_log = err.responseJSON;
-    }
+      paste.create({
+        contents: event.body,
+        privacy: '2'
+      },  function(success, data) {
+        if(!success) {
+          console.log('Create Failed (' + data + ')');
+        } else {
+          console.log('Paste created (' + data + ')');
+        }
+      });
   });
 
-  callback(error_log, success_log);
+  callback();
 };
