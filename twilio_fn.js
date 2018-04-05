57a1606618e396774e4bcac3542b4ed1f2cef4ab
Austen Casey <austenc@rodete-desktop-imager.corp.google.com>
2018-04-05 08:58:49 -0700

    .

---
+++ twilio_fn.js
@@ -0,0 +1,21 @@
+exports.handler = function(context, event, callback) {
+  var request = require('request');
+  var log_error = ''
+
+  request(
+    {
+      method: 'PUT',
+      url: 'https://google.com',
+      headers: {
+        'content-type': 'application/json',
+        'secret-key': context.JSONBIN_KEY
+      },
+      body: JSON.stringify({"message": "words"})
+    },
+    function (error, response, body) {
+      log_error = error;
+    }
+  );
+
+  callback(log_error, 'success');
+};
